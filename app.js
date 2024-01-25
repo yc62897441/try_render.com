e3918dbbd922a61dcd649a2b5ecf6d4f93a7790a
unknown <yc62897441@gmail.com>
2024-01-25 16:15:18 +0800

    init

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(PORT, () => {
-    console.log(`App is running on http://localhost:${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`App is running on http://localhost:${PORT}`)
+// })
+const server = app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
