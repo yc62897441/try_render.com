79942d26eea040e5d243fda6f039e6d8ef94dbae
unknown <yc62897441@gmail.com>
2024-01-25 17:18:25 +0800

    init

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// 解析請求的資料(body)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // 跨域請求設定
 const cors = require('cors')
 const corsOptions = {
@@ -14,9 +18,25 @@ app.use(cors(corsOptions))
 const PORT = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-    console.log('req', req)
-    console.log('res', res)
+    console.log('========')
+    console.log('========')
+    console.log('========')
+    console.log('get req.body', req.body)
+    console.log('get req.params', req.params)
+    console.log('get req.query', req.query)
+
     res.send('hello world')
+})
+
+app.post('/', (req, res) => {
+    console.log('========')
+    console.log('========')
+    console.log('========')
+    console.log('post req.body', req.body)
+    console.log('post req.params', req.params)
+    console.log('post req.query', req.query)
+
+    res.json({ b: 'b' })
 })
 
 // const server = app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
