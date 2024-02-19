090811cff43f45193bb772f1a532013ea7dd7f77
unknown <yc62897441@gmail.com>
2024-02-19 14:14:11 +0800

    feat: 新增餐廳資訊

--- routes/apis.js
+++ routes/apis.js
@@ -3,12 +3,28 @@ const express = require('express')
 const router = express.Router()
 
 const catsData = require('../dummyData/catsData')
+const restaurantsData = require('../dummyData/restaurantsData')
 
 router.post('/catslist', async (req, res) => {
     try {
         const offset = req.body.offset || 0
         const limit = offset + (req.body.limit || 10)
         const result = { catsData: catsData.slice(offset, limit) }
+
+        // 將查詢結果回傳給前端
+        res.json(result)
+    } catch (error) {
+        // 處理錯誤情況
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+router.post('/restaurantlist', async (req, res) => {
+    try {
+        const offset = req.body.offset || 0
+        const limit = offset + (req.body.limit || 10)
+        const result = { restaurantsData: restaurantsData.slice(offset, limit) }
 
         // 將查詢結果回傳給前端
         res.json(result)
