71859a7a55251fcc0c71543a678ad6ff575e11fa
unknown <yc62897441@gmail.com>
2024-03-14 11:47:43 +0800

    feat: 新增「不須權限的系統使用資料」的路由，以及北台灣行政區資料

---
+++ controllers/defaultController.js
@@ -0,0 +1,23 @@
+// 引入套件
+const express = require('express')
+const router = express.Router()
+
+// 行政區資料
+const northTaiwanDistricts = require('../dummyData/districts')
+
+// 取得北台灣行政區資料
+router.get('/userController', async (req, res) => {
+    try {
+        res.json({
+            status: 'success',
+            message: '',
+            data: northTaiwanDistricts,
+        })
+    } catch (error) {
+        // 處理錯誤情況
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-// 引入路由
-const routes = require('./routes')
-const apis = require('./apis')
-
 // passport 驗證
 const passport = require('../config/passport')
 const authenticated = passport.authenticate('jwt', { session: false })
 
-const userController = require('../controllers/userController')
+// 引入路由
+const routes = require('./routes')
+const apis = require('./apis')
+const userController = require('../controllers/userController') // user 權限相關
+const defaultController = require('../controllers/defaultController') // 不須權限的系統使用資料
 
 module.exports = (app, passport) => {
     // 無須驗證的路由
     app.post('/auth/signin', userController.signIn)
+    app.use('/default', defaultController)
 
     // 需要驗證的路由
     app.use('/api', authenticated, apis)
