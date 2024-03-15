e3e14328f293742556ac766dfaa145b8f00a7330
unknown <yc62897441@gmail.com>
2024-03-15 09:15:07 +0800

    feat: 新增「更新訂單狀態」路由

--- routes/apis.js
+++ routes/apis.js
@@ -38,34 +38,63 @@ router.post('/restaurantlist', async (req, res) => {
 })
 
 router.post('/orders', async (req, res) => {
-    // 縣市對照表
-    const table = {
-        0: '台北市',
-        1: '新北市',
-        2: '桃園市',
-        3: '新竹縣',
-        4: '新竹市',
-        5: '基隆市',
-        6: '宜蘭市',
-    }
-    const tempOrders = []
-    orders.forEach((order) => {
-        const obj = {
-            ...order,
-            city: table[order.city], // 轉化為縣市
-            district: northTaiwanDistricts.northTaiwanDistrictsTable[order.city][order.district], // 轉化為行政區
-        }
-        tempOrders.push(obj)
-    })
-
     try {
+        // 縣市對照表
+        const table = {
+            0: '台北市',
+            1: '新北市',
+            2: '桃園市',
+            3: '新竹縣',
+            4: '新竹市',
+            5: '基隆市',
+            6: '宜蘭市',
+        }
+        const tempOrders = []
+
+        // TODO: 等待正式導入資料庫
+        orders.forEach((order) => {
+            const obj = {
+                ...order,
+                city: table[order.city], // 轉化為縣市
+                district:
+                    northTaiwanDistricts.northTaiwanDistrictsTable[order.city][order.district], // 轉化為行政區
+            }
+            tempOrders.push(obj)
+        })
+
         res.json({
             status: 'success',
             message: '',
             data: tempOrders,
         })
     } catch (error) {
-        // 處理錯誤情況
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+router.put('/orders', async (req, res) => {
+    try {
+        const { orderId, submitType } = req.body
+        let isUpdateCompleted = false
+        orders.forEach((order) => {
+            if (order.id === orderId) {
+                // TODO: 等待正式導入資料庫
+                order.status =
+                    submitType === 'confirmOrder'
+                        ? 1
+                        : submitType === 'completeOrder'
+                        ? 2
+                        : submitType === 'cancelOrder'
+                        ? 3
+                        : ''
+                isUpdateCompleted = true
+            }
+        })
+        res.json({
+            status: isUpdateCompleted ? 'success' : 'fail',
+            message: isUpdateCompleted ? '訂單更新成功' : '訂單更新失敗',
+        })
+    } catch (error) {
         console.error(error)
         res.status(500).json({ error: 'Internal Server Error' })
     }
